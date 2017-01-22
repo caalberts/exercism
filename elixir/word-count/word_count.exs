042e274da043868a63364931f81795bca5f16fac
Albert Salim <albertlimca@gmail.com>
2017-01-22 22:30:49 +0800

    update elixir word-count

--- elixir/word-count/word_count.exs
+++ elixir/word-count/word_count.exs
@@ -7,32 +7,9 @@ defmodule Words do
   @spec count(String.t) :: map
   def count(sentence) do
     sentence
-    |> exclude_punctuations
-    |> count_words
-  end
-
-  defp exclude_punctuations(words) do
-    words
-    |> String.split("")
-    |> Enum.filter(&(allowed_character?(&1)))
-    |> Enum.join("")
-  end
-
-  defp allowed_character?(char) do
-    Regex.match?(~r/\w|\s|\-|\_/, char)
-  end
-
-  defp count_words(sentence) do
-    sentence
-    |> String.split([" ", "_"])
-    |> Enum.reduce(%{}, &counter/2)
-  end
-
-  defp counter(word, count) do
-    if String.length(word) > 0 do
-      Dict.update(count, String.downcase(word), 1, &(&1 + 1))
-    else
-      count
-    end
+    |> String.downcase
+    |> String.split(~r/\s|_|\W\W/, trim: true)
+    |> Enum.reduce(%{}, fn(word, counter) -> Map.update(counter, word, 1, &(&1 + 1))
+    end)
   end
 end
