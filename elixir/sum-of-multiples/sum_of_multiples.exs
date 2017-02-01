514ad2aafeb7a67a90f9eb85d662f58350bfad0f
Albert Salim <albertlimca@gmail.com>
2017-02-02 00:16:31 +0800

    elixir sum of multiples

---
+++ elixir/sum-of-multiples/sum_of_multiples.exs
@@ -0,0 +1,15 @@
+defmodule SumOfMultiples do
+  @doc """
+  Adds up all numbers from 1 to a given end number that are multiples of the factors provided.
+  """
+  @spec to(non_neg_integer, [non_neg_integer]) :: non_neg_integer
+  def to(limit, factors) do
+    1..limit-1
+    |> Enum.filter(&(is_multiple?(&1, factors)))
+    |> Enum.reduce(&(&1 + &2))
+  end
+
+  defp is_multiple?(num, []), do: false
+  defp is_multiple?(num, [head | tail]) when rem(num, head) == 0, do: true
+  defp is_multiple?(num, [_ | tail]), do: is_multiple?(num, tail)
+end
