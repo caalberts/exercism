e84665f772f658f958fef8ebd0e0b62bc55dac54
Albert Salim <albertlimca@gmail.com>
2018-03-10 23:28:29 +0800

    simplify raindrops

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,39 +1,23 @@
 package raindrops
 
-import (
-  "fmt"
-  "math"
-  "bytes"
-)
-
-type Matcher func(int, *bytes.Buffer) *bytes.Buffer
-
-var matchers = []Matcher{
-  matcherFactory(3, "Pling"),
-  matcherFactory(5, "Plang"),
-  matcherFactory(7, "Plong"),
-}
+import "strconv"
 
 func Convert(n int) string {
-  var buffer bytes.Buffer
+  var buffer string
 
-  for _, matcher := range matchers {
-    matcher(n, &buffer)
+  if n%3 == 0 {
+    buffer += "Pling"
+  }
+  if n%5 == 0 {
+    buffer += "Plang"
+  }
+  if n%7 == 0 {
+    buffer += "Plong"
   }
 
-  if buffer.Len() > 0 {
-    return buffer.String()
+  if len(buffer) == 0 {
+    return strconv.Itoa(n)
   }
 
-  return fmt.Sprintf("%d", n)
-}
-
-func matcherFactory(divisor int, addition string) Matcher {
-  return func(n int, buffer *bytes.Buffer) *bytes.Buffer {
-    if math.Remainder(float64(n), float64(divisor)) == 0 {
-      buffer.WriteString(addition)
-    }
-    
-    return buffer
-  }
+  return buffer
 }
