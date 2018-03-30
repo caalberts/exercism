30a2f746d2f9bad740d11c548cc5d0628c0933ee
Albert Salim <albertlimca@gmail.com>
2018-03-30 17:28:17 +0800

    go difference of squares - pipelines

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -1,25 +1,54 @@
 package diffsquares
 
 func SquareOfSums(n int) int {
-	sum := 0
-
-	for i := 1; i <= n; i++ {
-		sum += i
-	}
-
-	return sum * sum
+	return <-square(sum(generate(n)))
 }
 
 func SumOfSquares(n int) int {
-	sum := 0
-
-	for i := 1; i <= n; i++ {
-		sum += i * i
-	}
-
-	return sum
+	return <-sum(square(generate(n)))
 }
 
 func Difference(n int) int {
 	return SquareOfSums(n) - SumOfSquares(n)
+}
+
+func generate(n int) <-chan int {
+	out := make(chan int, n)
+
+	go func() {
+		for i := 1; i <= n; i++ {
+			out <- i
+		}
+		close(out)
+	}()
+
+	return out
+}
+
+func sum(in <-chan int) <-chan int {
+	out := make(chan int, 100)
+
+	go func() {
+		sum := 0
+		for num := range in {
+			sum += num
+		}
+		out <- sum
+		close(out)
+	}()
+
+	return out
+}
+
+func square(in <-chan int) <-chan int {
+	out := make(chan int, 100)
+
+	go func() {
+		for num := range in {
+			out <- num * num
+		}
+		close(out)
+	}()
+
+	return out
 }
