360bf834c683a850c045b97966eb3994104526be
Albert Salim <albertlimca@gmail.com>
2018-03-30 16:09:19 +0800

    gofmt

--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -8,17 +8,17 @@ const EarthOrbit = 31557600
 
 // RelativeOrbit of planets relative to Earth
 var RelativeOrbit = map[Planet]float64{
-  "Earth": 1.0,
-  "Mercury": 0.2408467,
-  "Venus": 0.61519726,
-  "Mars": 1.8808158,
-  "Jupiter": 11.862615,
-  "Saturn": 29.447498,
-  "Uranus": 84.016846,
-  "Neptune": 164.79132,
+	"Earth":   1.0,
+	"Mercury": 0.2408467,
+	"Venus":   0.61519726,
+	"Mars":    1.8808158,
+	"Jupiter": 11.862615,
+	"Saturn":  29.447498,
+	"Uranus":  84.016846,
+	"Neptune": 164.79132,
 }
 
 // Age calculates someone's age on a planet
 func Age(seconds float64, planet Planet) float64 {
-  return seconds / EarthOrbit / RelativeOrbit[planet]
+	return seconds / EarthOrbit / RelativeOrbit[planet]
 }
