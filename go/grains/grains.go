18c98b20b73ccd3570991a7e7b0013dfcbca4b92
Albert Salim <albertlimca@gmail.com>
2018-03-31 15:18:47 +0700

    go grains - recursive & memoised

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -2,17 +2,29 @@ package grains
 
 import (
 	"errors"
-	"math"
 )
+
+var cache = make(map[int]uint64)
 
 func Square(n int) (uint64, error) {
 	if n <= 0 || n > 64 {
 		return 0, errors.New("n must be between 1 and 64 inclusive")
 	}
 
-	exp := float64(n - 1)
-	square := uint64(math.Pow(2, exp))
-	return square, nil
+	return square(n), nil
+}
+
+func square(n int) uint64 {
+	if n == 1 {
+		return 1
+	}
+
+	if square, exists := cache[n]; exists {
+		return square
+	}
+
+	cache[n] = square(n-1) * 2
+	return cache[n]
 }
 
 func Total() uint64 {
