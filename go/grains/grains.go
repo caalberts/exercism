8b8e499eda8afb627905ae8ac9149c9734a70de6
Albert Salim <albertlimca@gmail.com>
2018-03-31 15:52:56 +0700

    go grains - use bit shift & geometric progression

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -4,34 +4,22 @@ import (
 	"errors"
 )
 
-var cache = make(map[int]uint64)
-
 func Square(n int) (uint64, error) {
 	if n <= 0 || n > 64 {
 		return 0, errors.New("n must be between 1 and 64 inclusive")
 	}
 
-	return square(n), nil
+	return powerOfTwo(n - 1), nil
 }
 
-func square(n int) uint64 {
-	if n == 1 {
-		return 1
-	}
-
-	if square, exists := cache[n]; exists {
-		return square
-	}
-
-	cache[n] = square(n-1) * 2
-	return cache[n]
+func powerOfTwo(n int) uint64 {
+	return 1 << uint(n)
 }
 
 func Total() uint64 {
-	var sum uint64
-	for n := 1; n <= 64; n++ {
-		square, _ := Square(n)
-		sum += square
-	}
-	return sum
+	// Sum of geometric progression
+	// 2^0 + 2^1 + 2^2 + ... + 2^63
+	// 1 + 2x (1 - 2^63) / (1 - 2)
+	// 1 - 2 + 2^64
+	return powerOfTwo(64) - 1
 }
